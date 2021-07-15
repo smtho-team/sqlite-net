96a878875853a609b39e228fda5b7b85d7edf458
bigbaldy <bigbaldy@163.com>
2021-07-15 11:50:17 +0800

    improve converter

--- tests/SQLite.Tests/ConverterTest.cs
+++ tests/SQLite.Tests/ConverterTest.cs
@@ -8,7 +8,7 @@ namespace SQLite.Tests
 {
 	public class ConverterTest
 	{
-		public class ListStringConverter : Converter<string, List<string>>
+		public class ListStringConverter : IConverter<string, List<string>>
 		{
 			public string Convert (List<string> t)
 			{
@@ -25,12 +25,12 @@ namespace SQLite.Tests
 		{
 			[PrimaryKey, AutoIncrement]
 			public int Id { get; set; }
-			[Column ("Readers", typeof (ListStringConverter))]
+			[Column ("Readers", Converter = typeof (ListStringConverter))]
 			public List<string> Readers { get; set; }
 		}
 
 		[Test]
-		public void Test ()
+		public void Insert ()
 		{
 			var databasePath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "MyData.db");
 			var db = new SQLiteConnection (databasePath);
@@ -40,6 +40,28 @@ namespace SQLite.Tests
 			};
 			db.Insert (stock);
 			List<Stock> stocks = db.Table<Stock> ().ToList ();
+			db.DropTable<Stock> ();
+			Assert.AreEqual (4, stocks[0].Readers.Count);
+		}
+
+		[Test]
+		public void Update ()
+		{
+			var databasePath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "MyData.db");
+			var db = new SQLiteConnection (databasePath);
+			db.CreateTable<Stock> ();
+			Stock stock = new Stock () {
+				Readers = new List<string> { "1", "2", "3", "4" }
+			};
+			db.Insert (stock);
+			stock = new Stock () {
+				Id = 1,
+				Readers = new List<string> { "5", "2", "3", "4" }
+			};
+			db.Update (stock);
+			List<Stock> stocks = db.Table<Stock> ().ToList ();
+			db.DropTable<Stock> ();
+			Assert.AreEqual ("5", stocks[0].Readers[0]);
 		}
 	}
 }
